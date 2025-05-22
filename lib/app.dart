7182bfc32276201477ba996a732b1aa98f373edc
KrisNathan <kristophernathanael2005@gmail.com>
2025-05-22 18:08:09 +0700

    feat: navigation bar
    Currently not functional, just a mockup.

--- lib/app.dart
+++ lib/app.dart
@@ -9,6 +9,28 @@ class AppWidget extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('LinguaScreen')),
       body: HomePage(),
+
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {},
+        selectedIndex: 1,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.book),
+            icon: Icon(Icons.book_outlined),
+            label: 'Dictionary',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.quiz),
+            icon: Icon(Icons.quiz_outlined),
+            label: 'Flashcards',
+          ),
+        ],
+      ),
     );
   }
 }
